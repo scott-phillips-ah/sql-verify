a61e6929982b40ace307d88126e36ddd3ff40cf3
Scott Phillips <57362841+scott-phillips-ah@users.noreply.github.com>
2019-12-23 11:06:58 -0500

    Initial code file commit

---
+++ SQLVerify/SQLVerify/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SQLVerify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = Antlr4
+        }
+    }
+}
